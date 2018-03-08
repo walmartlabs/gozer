bb3928f727aa3200832c649c03b2d4a0625dbbe5
mbarlot <mike.barlotta@walmart.com>
2018-03-08 18:19:38 -0500

    TODO for tests

--- src/test/java/com/walmartlabs/x12/dex/dx894/DefaultDex894ValidatorTest.java
+++ src/test/java/com/walmartlabs/x12/dex/dx894/DefaultDex894ValidatorTest.java
@@ -2,13 +2,11 @@ package com.walmartlabs.x12.dex.dx894;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class DefaultDex894ValidatorTest {
-    
+
     @Test
     public void test() {
-        fail("Not yet implemented");
+        // TODO
     }
-    
+
 }
