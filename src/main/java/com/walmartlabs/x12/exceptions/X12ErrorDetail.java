29b4bdbbe9ffda2747bfa8bf2f65d071c7d470b0
mbarlot <mike.barlotta@walmart.com>
2018-03-21 10:05:46 -0400

    Fix toString end of line in X12ErrorDetails

--- src/main/java/com/walmartlabs/x12/exceptions/X12ErrorDetail.java
+++ src/main/java/com/walmartlabs/x12/exceptions/X12ErrorDetail.java
@@ -53,11 +53,10 @@ public class X12ErrorDetail {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("DEX error:");
-        sb.append("\nsegmentId: ").append(segmentId);
-        sb.append("\nelementId: ").append(elementId);
-        sb.append("\nline number: ").append(lineNumber);
-        sb.append("\nmessage: ").append(message);
+        sb.append("\\nsegmentId: ").append(segmentId);
+        sb.append("\\nelementId: ").append(elementId);
+        sb.append("\\nline number: ").append(lineNumber);
+        sb.append("\\nmessage: ").append(message);
         return sb.toString();
     }
-
 }
