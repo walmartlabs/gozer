e279453aaac1879302f830a2e8ec1e3ca12e93d3
CodeSmell <mbarlotta@gmail.com>
2018-04-09 16:14:01 -0400

    Issue #1 use Generics in interfaces

--- src/main/java/com/walmartlabs/x12/dex/dx894/DefaultDex894Parser.java
+++ src/main/java/com/walmartlabs/x12/dex/dx894/DefaultDex894Parser.java
@@ -15,6 +15,7 @@ limitations under the License.
  */
 package com.walmartlabs.x12.dex.dx894;
 
+import com.walmartlabs.x12.X12Parser;
 import com.walmartlabs.x12.exceptions.X12ErrorDetail;
 import com.walmartlabs.x12.exceptions.X12ParserException;
 import com.walmartlabs.x12.util.VersionUtil;
@@ -51,8 +52,21 @@ import java.util.List;
  * -- SE
  *
  */
-public class DefaultDex894Parser implements Dex894Parser {
+public class DefaultDex894Parser implements X12Parser {
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultDex894Parser.class);
+
+    public static final String APPLICATION_HEADER_ID = "DXS";
+    public static final String APPLICATION_TRAILER_ID = "DXE";
+    public static final String TRANSACTION_SET_HEADER_ID = "ST";
+    public static final String TRANSACTION_SET_TRAILER_ID = "SE";
+    public static final String G82_ID = "G82";
+    public static final String G83_ID = "G83";
+    public static final String G72_ID = "G72";
+    public static final String G84_ID = "G84";
+    public static final String G85_ID = "G85";
+    public static final String G86_ID = "G86";
+    public static final String LOOP_HEADER_ID = "LS";
+    public static final String LOOP_TRAILER_ID = "LE";
 
     /**
      * parse the DEX 894 transmission into
