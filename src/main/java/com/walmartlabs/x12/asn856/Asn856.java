840ea5a0714f32202ea2d44c43069875b66472b6
CodeSmell <mbarlotta@gmail.com>
2019-01-29 15:12:53 -0500

    ASN 856 stub

---
+++ src/main/java/com/walmartlabs/x12/asn856/Asn856.java
@@ -0,0 +1,12 @@
+package com.walmartlabs.x12.asn856;
+
+import com.walmartlabs.x12.X12Document;
+
+public class Asn856 implements X12Document {
+
+    @Override
+    public String getX12DocumentType() {
+        return null;
+    }
+
+}
