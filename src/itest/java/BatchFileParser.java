0c18b2cf679cd89ed1ad8aa6a5d887e7fe449d38
codesmell <mbarlotta@gmail.com>
2019-08-29 18:22:17 -0400

    cleanup util so uses arg instead of hardcoded path

--- src/itest/java/BatchFileParser.java
+++ src/itest/java/BatchFileParser.java
@@ -18,17 +18,29 @@ public class BatchFileParser {
     private static final StandardX12Parser x12Parser = new StandardX12Parser();
     
     public static void main(String[] args) throws IOException {
-        // get list of files in input folder
-        String inputDirectory = "/Users/mbarlot/edi_files/storeASN";
-        Path inputFolder = Paths.get(inputDirectory);
-        Path okFolder = Paths.get(inputDirectory + "/success");
-        Path rejectFolder = Paths.get(inputDirectory + "/failed");
+        if (args != null && args.length > 0) {
+            // get list of files in input folder
+            String inputDirectory = args[0];
+            Path inputFolder = Paths.get(inputDirectory);
+
+            if (Files.exists(inputFolder)) {
+                Path okFolder = Paths.get(inputDirectory + "/success");
+                Path rejectFolder = Paths.get(inputDirectory + "/failed");
+                processDirectory(inputFolder, okFolder, rejectFolder);
+            } else {
+                LOGGER.warn("the input folder does not exist");
+            }
+        } else {
+            LOGGER.warn("please provide an input folder as an argument");
+        }
+    }
         
+    private static void processDirectory(Path inputFolder, Path okFolder, Path rejectFolder) throws IOException {
         AtomicInteger successCount = new AtomicInteger(0);
         AtomicInteger failedCount = new AtomicInteger(0);
         
-        Files.list(inputFolder)
-            .filter(Files::isRegularFile)
+        try (Stream<Path> files = Files.list(inputFolder)) {
+            files.filter(Files::isRegularFile)
             .forEach(sourceFile -> {
                 boolean isSuccess = parseFile(sourceFile, okFolder, rejectFolder);
                 if (isSuccess) {
@@ -37,11 +49,12 @@ public class BatchFileParser {
                     failedCount.incrementAndGet();
                 }
             });
+        }
         
         LOGGER.info("Parsed files - successful {}, failed {}", successCount, failedCount);
     }
 
-    public static boolean parseFile(Path sourceFile, Path okFolder, Path rejectFolder) {
+    private static boolean parseFile(Path sourceFile, Path okFolder, Path rejectFolder) {
         boolean isSuccess = false;
         
         try {
@@ -59,16 +72,14 @@ public class BatchFileParser {
             copyFile(sourceFile, rejectFolder);
             // write file w/ parsing fail reason
             writeReasonFile(sourceFile, rejectFolder, e.getMessage());
-        } catch (UncheckedIOException e) {
-            writeReasonFile(sourceFile, rejectFolder, e.getMessage());
-        } catch (IOException e) {
+        } catch (UncheckedIOException | IOException e) {
             writeReasonFile(sourceFile, rejectFolder, e.getMessage());
         }
         
         return isSuccess;
     }
 
-    public static String readFile(Path sourceFile) throws IOException, UncheckedIOException {
+    private static String readFile(Path sourceFile) throws IOException, UncheckedIOException {
         // read the file
         String content = null;
         try (Stream<String> lines = Files.lines(sourceFile)) {
@@ -77,7 +88,7 @@ public class BatchFileParser {
         return content;
     }
     
-    public static void writeReasonFile(Path sourceFile, Path rejectFolder, String errReason) {
+    private static void writeReasonFile(Path sourceFile, Path rejectFolder, String errReason) {
         try {
             Path errFile = rejectFolder.resolve(sourceFile.getFileName() + ".reason.txt");
             Files.write(errFile, errReason.getBytes());
@@ -86,7 +97,7 @@ public class BatchFileParser {
          }
     }
     
-    public static void copyFile(Path fileToCopy, Path folder) {
+    private static void copyFile(Path fileToCopy, Path folder) {
         try {
             if (!Files.exists(folder)) {
                 Files.createDirectories(folder);
