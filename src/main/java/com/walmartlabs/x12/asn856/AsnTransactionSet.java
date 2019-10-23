0b7048d8d9ccbd72fd091d9915a406c3ccb6b90a
Mike Barlotta <CodeSmell@users.noreply.github.com>
2019-10-23 15:32:56 -0400

    ASN transaction set parsing (partial implementation) (#58)

--- src/main/java/com/walmartlabs/x12/asn856/AsnTransactionSet.java
+++ src/main/java/com/walmartlabs/x12/asn856/AsnTransactionSet.java
@@ -19,13 +19,85 @@ import com.walmartlabs.x12.AbstractX12TransactionSet;
 
 public class AsnTransactionSet extends AbstractX12TransactionSet {
 
-    private String sampleAsnOnly;
+    //
+    // BSN
+    // 
+    // BSN 01
+    private String purposeCode;
+    // BSN 02
+    private String shipmentIdentification;
+    // BSN 03
+    private String shipmentDate; // CCYYMMDD
+    // BSN 04
+    private String shipmentTime;
+    // BSN 05
+    private String hierarchicalStructureCode;
+    
+    // HL (Shipment)
+    Shipment shipment;
+    
+    //
+    // CTT
+    //
+    // CTT 01
+    private Integer transactionLineItems;
 
-    public String getSampleAsnOnly() {
-        return sampleAsnOnly;
+    
+    
+    public String getPurposeCode() {
+        return purposeCode;
     }
 
-    public void setSampleAsnOnly(String sampleAsnOnly) {
-        this.sampleAsnOnly = sampleAsnOnly;
+    public void setPurposeCode(String purposeCode) {
+        this.purposeCode = purposeCode;
     }
+
+    public String getShipmentIdentification() {
+        return shipmentIdentification;
+    }
+
+    public void setShipmentIdentification(String shipmentIdentification) {
+        this.shipmentIdentification = shipmentIdentification;
+    }
+
+    public String getShipmentDate() {
+        return shipmentDate;
+    }
+
+    public void setShipmentDate(String shipmentDate) {
+        this.shipmentDate = shipmentDate;
+    }
+
+    public String getShipmentTime() {
+        return shipmentTime;
+    }
+
+    public void setShipmentTime(String shipmentTime) {
+        this.shipmentTime = shipmentTime;
+    }
+
+    public String getHierarchicalStructureCode() {
+        return hierarchicalStructureCode;
+    }
+
+    public void setHierarchicalStructureCode(String hierarchicalStructureCode) {
+        this.hierarchicalStructureCode = hierarchicalStructureCode;
+    }
+
+    public Shipment getShipment() {
+        return shipment;
+    }
+
+    public void setShipment(Shipment shipment) {
+        this.shipment = shipment;
+    }
+
+    public Integer getTransactionLineItems() {
+        return transactionLineItems;
+    }
+
+    public void setTransactionLineItems(Integer transactionLineItems) {
+        this.transactionLineItems = transactionLineItems;
+    }
+    
 }
--- src/main/java/com/walmartlabs/x12/asn856/DefaultAsn856TransactionSetParser.java
+++ src/main/java/com/walmartlabs/x12/asn856/DefaultAsn856TransactionSetParser.java
@@ -19,6 +19,8 @@ import com.walmartlabs.x12.X12Segment;
 import com.walmartlabs.x12.X12TransactionSet;
 import com.walmartlabs.x12.standard.AbstractTransactionSetParserChainable;
 import com.walmartlabs.x12.standard.X12Group;
+import com.walmartlabs.x12.standard.X12Loop;
+import com.walmartlabs.x12.util.ConversionUtil;
 import com.walmartlabs.x12.util.X12ParsingUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,34 +34,156 @@ import java.util.List;
  *
  */
 public class DefaultAsn856TransactionSetParser extends AbstractTransactionSetParserChainable {
-    
+
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultAsn856TransactionSetParser.class);
-    
+
     public static final String ASN_TRANSACTION_TYPE = "856";
     public static final String ASN_TRANSACTION_HEADER = "BSN";
+    public static final String ASN_TRANSACTION_TOTALS = "CTT";
 
+    @Override
     protected boolean handlesTransactionSet(List<X12Segment> transactionSegments, X12Group x12Group) {
         // the first segment should be an ST with the 856 transaction type code
         return X12ParsingUtil.verifyTransactionSetType(transactionSegments, ASN_TRANSACTION_TYPE);
     }
 
+    /**
+     * it is assumed that this method is only called after getting true as a
+     * response from {@link handlesTransactionSet}
+     */
+    @Override
     protected X12TransactionSet doParse(List<X12Segment> transactionSegments, X12Group x12Group) {
         AsnTransactionSet asnTx = null;
 
-        if (transactionSegments != null) {
-            // MUST be wrapped in a valid transaction set envelope
-            if (X12ParsingUtil.isValidEnvelope(transactionSegments, 
-                X12TransactionSet.TRANSACTION_SET_HEADER,
-                X12TransactionSet.TRANSACTION_SET_TRAILER)) {
-
-                // TODO: make sure first segment is a BSN
-
-                asnTx = new AsnTransactionSet();
-                asnTx.setSampleAsnOnly("TEST");
-            }
+        if (transactionSegments != null && transactionSegments.size() > 0) {
+            asnTx = new AsnTransactionSet();
+            
+            this.doParsing(transactionSegments, asnTx);
         }
-        
+
         return asnTx;
     }
 
+    protected void doParsing(List<X12Segment> transactionSegments, AsnTransactionSet asnTx) {
+        int segmentCount = transactionSegments.size();
+        
+        //
+        // ST
+        //
+        X12Segment currentSegment = transactionSegments.get(0);
+        this.parseTransactionSetHeader(currentSegment, asnTx);
+
+        //
+        // BSN
+        //
+        currentSegment = transactionSegments.get(1);
+        this.parseBeginningSegment(currentSegment, asnTx);
+        
+        //
+        // Hierarchical Loops
+        // 
+
+        int segementAfterHierarchicalLoops = this.findSegmentAfterHierarchicalLoops(transactionSegments);
+        List<X12Segment> loopSegments = transactionSegments.subList(2, segementAfterHierarchicalLoops);
+        List<X12Loop> loops = X12ParsingUtil.findHierarchicalLoops(loopSegments);
+        // TODO: add loop parsing
+        
+        //
+        // CTT (optional)
+        //
+        currentSegment = transactionSegments.get(segementAfterHierarchicalLoops);
+        if (ASN_TRANSACTION_TOTALS.equals(currentSegment.getSegmentIdentifier())) {
+            this.parseTransactionTotals(currentSegment, asnTx);
+            currentSegment = transactionSegments.get(segmentCount - 1);
+        }
+        
+        //
+        // SE
+        //
+        this.parseTransactionSetTrailer(currentSegment, asnTx);
+    }
+
+    private int findSegmentAfterHierarchicalLoops(List<X12Segment> transactionSegments) {
+        int segmentCount = transactionSegments.size();
+        int secondToLastSegmentIndex = segmentCount - 2;
+        X12Segment segment = transactionSegments.get(secondToLastSegmentIndex);
+        // 2nd to last line is CTT (optional) otherwise it is part of HL
+        if (ASN_TRANSACTION_TOTALS.equals(segment.getSegmentIdentifier())) {
+            // CTT segment
+            return secondToLastSegmentIndex;
+        } else {
+            // SE segment
+            return segmentCount - 1;
+        }
+    }
+    
+    /**
+     * parse the ST segment
+     * @param segment
+     * @param asnTx
+     */
+    private void parseTransactionSetHeader(X12Segment segment, AsnTransactionSet asnTx) {
+        LOGGER.debug(segment.getSegmentIdentifier());
+
+        String segmentIdentifier = segment.getSegmentIdentifier();
+        if (X12TransactionSet.TRANSACTION_SET_HEADER.equals(segmentIdentifier)) {
+            asnTx.setTransactionSetIdentifierCode(segment.getSegmentElement(1));
+            asnTx.setHeaderControlNumber(segment.getSegmentElement(2));
+        } else {
+            throw X12ParsingUtil.handleUnexpectedSegment(X12TransactionSet.TRANSACTION_SET_HEADER, segmentIdentifier);
+        }
+    }
+    
+    /**
+     * parse the SE segment
+     * @param segment
+     * @param asnTx
+     */
+    private void parseTransactionSetTrailer(X12Segment segment, AsnTransactionSet asnTx) {
+        LOGGER.debug(segment.getSegmentIdentifier());
+
+        String segmentIdentifier = segment.getSegmentIdentifier();
+        if (X12TransactionSet.TRANSACTION_SET_TRAILER.equals(segmentIdentifier)) {
+            asnTx.setExpectedNumberOfSegments(ConversionUtil.convertStringToInteger(segment.getSegmentElement(1)));
+            asnTx.setTrailerControlNumber(segment.getSegmentElement(2));
+        } else {
+            throw X12ParsingUtil.handleUnexpectedSegment(X12TransactionSet.TRANSACTION_SET_TRAILER, segmentIdentifier);
+        }
+    }
+
+    /**
+     * parse the BSN segment
+     * @param segment
+     * @param asnTx
+     */
+    private void parseBeginningSegment(X12Segment segment, AsnTransactionSet asnTx) {
+        LOGGER.debug(segment.getSegmentIdentifier());
+        
+        String segmentIdentifier = segment.getSegmentIdentifier();
+        if (ASN_TRANSACTION_HEADER.equals(segmentIdentifier)) {
+            asnTx.setPurposeCode(segment.getSegmentElement(1));
+            asnTx.setShipmentIdentification(segment.getSegmentElement(2));
+            asnTx.setShipmentDate(segment.getSegmentElement(3));
+            asnTx.setShipmentTime(segment.getSegmentElement(4));
+            asnTx.setHierarchicalStructureCode(segment.getSegmentElement(5));
+        } else {
+            throw X12ParsingUtil.handleUnexpectedSegment(ASN_TRANSACTION_HEADER, segmentIdentifier);
+        }
+    }
+
+    /**
+     * parse the CTT segment
+     * @param segment
+     * @param asnTx
+     */
+    private void parseTransactionTotals(X12Segment segment, AsnTransactionSet asnTx) {
+        LOGGER.debug(segment.getSegmentIdentifier());
+
+        String segmentIdentifier = segment.getSegmentIdentifier();
+        if (ASN_TRANSACTION_TOTALS.equals(segmentIdentifier)) {
+            asnTx.setTransactionLineItems(ConversionUtil.convertStringToInteger(segment.getSegmentElement(1)));
+        } else {
+            throw X12ParsingUtil.handleUnexpectedSegment(ASN_TRANSACTION_TOTALS, segmentIdentifier);
+        }
+    }
 }
